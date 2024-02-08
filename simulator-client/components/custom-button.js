59eab1a3e5a1ebdd4105341f58ee17683571c7ae
sethlxk <sethlxk@gmail.com>
2024-02-08 22:31:40 +0800

    feat | refactor code

--- simulator-client/components/custom-button.js
+++ simulator-client/components/custom-button.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const customIcon = (
   <svg
@@ -20,34 +20,34 @@ const customIcon = (
 
 const CustomButton = ({ onClick, icon }) => {
   const buttonStyle = {
-    padding: '10px',
-    border: 'none',
-    backgroundColor: 'blue',
-    color: 'white',
-    cursor: 'pointer',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    opacity: '0'
+    padding: "10px",
+    border: "none",
+    backgroundColor: "blue",
+    color: "white",
+    cursor: "pointer",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: "0",
   };
 
   return (
     <button onClick={onClick} style={buttonStyle}>
       <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          width="20"
-          height="20"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        width="20"
+        height="20"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
     </button>
   );
 };
