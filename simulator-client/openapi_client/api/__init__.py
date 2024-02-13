229bceafb607c33b133db50b68a18112e8c2511b
sethlxk <sethlxk@gmail.com>
2024-02-13 14:20:30 +0800

    Revert "add openapi generated client"
    This reverts commit 8402a80b4b195a471d34449e19966660e138adb5.

--- simulator-client/openapi_client/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.image_recognition_api import ImageRecognitionApi
-from openapi_client.api.pathfinding_api import PathfindingApi
-
