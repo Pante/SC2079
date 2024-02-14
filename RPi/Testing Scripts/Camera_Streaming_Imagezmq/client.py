1a1b3d2e0e7370148df7d24bde6926c7949e422b
BryanTohWS <bryantws1999@gmail.com>
2024-02-14 22:50:28 +0800

    added imagezmq camera video streaming scripts

---
+++ RPi/Testing Scripts/Camera_Streaming_Imagezmq/client.py
@@ -0,0 +1,24 @@
+import time
+import io
+import imagezmq
+from picamera import PiCamera
+
+camera = PiCamera()
+# TODO: Replace the IP Address with the PC's IP Address
+image_sender = imagezmq.ImageSender(connect_to='tcp://999.999.999.999:5555')  
+
+camera.start_preview()
+
+try:
+    time.sleep(2)  # Give the camera time to warm up
+    while True:
+        stream = io.BytesIO()
+        camera.capture(stream, format='jpeg')
+        image_sender.send_image(stream.getvalue())
+        time.sleep(0.1)  # Adjust the delay based on your needs
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    stream.close()
+    image_sender.close()
+    camera.stop_preview()
