bf8735ecbcae27e6ad107b8da2c86f67f8e1c4f9
BryanTohWS <bryantws1999@gmail.com>
2024-02-17 17:14:45 +0800

    commit

--- RPi/Testing Scripts/Camera_Streaming_Imagezmq/client.py
+++ RPi/Testing Scripts/Camera_Streaming_Imagezmq/client.py
@@ -5,7 +5,7 @@ from picamera import PiCamera
 
 camera = PiCamera()
 # TODO: Replace the IP Address with the PC's IP Address
-image_sender = imagezmq.ImageSender(connect_to='tcp://999.999.999.999:5555')  
+image_sender = imagezmq.ImageSender(connect_to='tcp://192.168.14.14')  
 
 camera.start_preview()
 
@@ -17,7 +17,7 @@ try:
         image_sender.send_image(stream.getvalue())
         time.sleep(0.1)  # Adjust the delay based on your needs
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print("Error occured")
 finally:
     stream.close()
     image_sender.close()
