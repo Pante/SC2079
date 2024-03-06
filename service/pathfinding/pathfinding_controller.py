Fix turning calculation in algorithm (#79)
* I suck at math

* Fix turning algo

* Change neighbours to include longer straightlines

service/pathfinding/pathfinding_controller.py
@@ -100,7 +100,7 @@ class PathfindingResponseSegment(BaseModel):
 
     @classmethod
     def from_segment(cls, verbose: bool, segment: Segment):
-        cost = segment.cost if verbose else None
+        cost = segment.cost if verbose else 0
         instructions = [
             (
                 PathfindingResponseMoveInstruction.from_move_instruction(i)
@@ -112,7 +112,7 @@ class PathfindingResponseSegment(BaseModel):
         vectors = (
             [PathfindingVector.from_vector(vector) for vector in segment.vectors]
             if verbose
-            else None
+            else []
         )
         return cls(
             image_id=segment.image_id,
@@ -177,7 +177,7 @@ def pathfinding(body: PathfindingRequest):
         ]
     )
 
-    dump(world, segments)
+    # dump(world, segments)
     print(datetime.now())
 
     response = make_response(
@@ -196,9 +196,19 @@ def dump(world: World, segments: list[Segment]):
     :return:
     """
     map = np.array(world.grid, dtype=int)
+    for obstacle in world.obstacles:
+        west_x = max(obstacle.south_west.x, 0)
+        east_x = min(obstacle.north_east.x + 1, world.size)
+        south_y = max(obstacle.south_west.y, 0)
+        north_y = min(obstacle.north_east.y + 1, world.size)
+
+        map[west_x:east_x, south_y:north_y] = 9
+
     for i, s in enumerate(segments):
         for v in s.vectors:
             map[v.x, v.y] = i + 2
 
     a = np.rot90(map)
     np.savetxt("dump.txt", a, fmt="%d")
+
+# 10 cells