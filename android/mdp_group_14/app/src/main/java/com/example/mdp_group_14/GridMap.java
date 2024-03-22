10e1723721e0cb9589ce027c14a728b81632c14d
silverarrows22 <seow.melissa@gmail.com>
2024-03-22 11:14:33 +0800

    Revert "updates"
    This reverts commit 0c3e28ca91f58c5209ccdd44ca6a84c920c3514b.

--- android/mdp_group_14/app/src/main/java/com/example/mdp_group_14/GridMap.java
+++ android/mdp_group_14/app/src/main/java/com/example/mdp_group_14/GridMap.java
@@ -121,12 +121,12 @@ public class GridMap extends View{
         maroonPaint.setStrokeWidth(8);
         obstacleColor.setColor(getResources().getColor(R.color.black));
         imageColor.setColor(getResources().getColor(R.color.rockColor));
-        robotColor.setColor(getResources().getColor(R.color.pikaYellow));
+        robotColor.setColor(getResources().getColor(R.color.lightRed));
         robotColor.setStrokeWidth(2);
         endColor.setColor(Color.RED);
         startColor.setColor(Color.CYAN);
         waypointColor.setColor(Color.GREEN);
-        unexploredColor.setColor(getResources().getColor(R.color.skyBlue));
+        unexploredColor.setColor(getResources().getColor(R.color.grassGreen));
         exploredColor.setColor(getResources().getColor(R.color.exploredColor2));
         arrowColor.setColor(Color.BLACK);
         fastestPathColor.setColor(Color.MAGENTA);
